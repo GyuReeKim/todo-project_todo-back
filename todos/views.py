1d2bc27d85dc4eb31449350b9daa9ede71514fe7
GyuReeKim <starkim96@naver.com>
2019-11-19 13:48:19 +0900

    get put delete 완성

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import TodoSerializer
-# api_view는 view 페이지를 만들어준다.
+# api_view는 api view 페이지를 만들어준다.
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from .models import Todo
 
 
 # Create your views here.
@@ -21,3 +22,31 @@ def todo_create(request):
         return JsonResponse(serializer.data)
     # 오류가 있는지 확인한다.
     return HttpResponse(status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((JSONWebTokenAuthentication,))
+def todo_detail(request, id):
+    todo = get_object_or_404(Todo, id=id)
+
+    # read
+    if request.method == "GET":
+        # 직렬화 시킨다.
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+
+    # update
+    elif request.method == "PUT":
+        # rest framework에서 데이터를 가져오기 위해 request.data를 사용해 가져온다.
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        # 사용자가 실패했을 경우 어떤 페이지를 혹은 어떤 메세지를 보여주는 것이 좋을지 생각해야한다.
+        return HttpResponse(status=400)
+        
+    # delete
+    elif request.method == "DELETE":
+        todo.delete()
+        # return JsonResponse({"msg": "삭제되었습니다."})
+        return HttpResponse(status=204)
